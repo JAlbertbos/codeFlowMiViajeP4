be046680a1881ca252bd42ddac8233fc3c6020cc
Andrés Romero <146331142+aromerobai@users.noreply.github.com>
2023-12-10 17:44:39 +0000

    Update app.config.js and app.json

--- app.config.js
+++ app.config.js
@@ -1,28 +1,28 @@
 export default {
-    name: "codeflowmiviajep3",
-    slug: "codeflowmiviajep3",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./path/to/your/icon.png",
-    splash: {
-      image: "./path/to/your/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
+  name: "codeflowmiviajep3",
+  slug: "codeflowmiviajep3",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./path/to/your/icon.png",
+  splash: {
+    image: "./path/to/your/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./path/to/your/foreground.png",
+      backgroundColor: "#FFFFFF",
     },
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      supportsTablet: true
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./path/to/your/foreground.png",
-        backgroundColor: "#FFFFFF"
-      }
-    },
-    web: {
-      favicon: "./path/to/your/favicon.png"
-    }
-  };
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+};
