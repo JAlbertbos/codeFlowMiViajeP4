0058ca902ba139291ebbbf715747d4274db3d93d
Jorge A <101210883+JAlbertbos@users.noreply.github.com>
2023-12-16 15:39:54 +0100

    changes app ok

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ import { initializeApp } from 'firebase/app';
 
 // Configuración de Firebase, contiene información de autenticación y conexión a la base de datos
 const firebaseConfig = {
-    apiKey: "AIzaSyCBHtoO7naEQHJYFgioJDXYsLNDKJR-X74",
-    authDomain: "codeflowmiviajep3.firebaseapp.com",
-    projectId: "codeflowmiviajep3",
-    storageBucket: "codeflowmiviajep3.appspot.com",
-    messagingSenderId: "222002248733",
-    appId: "1:222002248733:web:5d46b01bd1bf7fe9e25af4",
-    measurementId: "G-ZBWNWPKLK4"
-  };
-  
+  apiKey: "AIzaSyAIxvIBjZFRaAKTg3IS8YQ4fXHfnQA_AC4",
+  authDomain: "codeflowmiviajep4.firebaseapp.com",
+  projectId: "codeflowmiviajep4",
+  storageBucket: "codeflowmiviajep4.appspot.com",
+  messagingSenderId: "239418664935",
+  appId: "1:239418664935:web:179126ec4a0f3809797f42",
+  measurementId: "G-JRF1F928L1"
+};
   // Inicializa la aplicación de Firebase con la configuración provista
   const app = initializeApp(firebaseConfig);
   
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addmessage = onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await getFirestore()
+      .collection("messages")
+      .add({original: original});
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
