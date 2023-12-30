575c9159d22ed08090983f784dee55f129d8a6fe
aromerobai <aromerobai@uoc.edu>
2023-12-30 17:23:00 +0100

    Cloud Messaging

--- App.js
+++ App.js
@@ -8,14 +8,70 @@ import DetailScreen from './src/screens/DetailScreen';
 import ListScreen from './src/screens/ListScreen';
 import PlayerScreen from './src/screens/PlayerScreen';
 
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
+
 // Crea un Stack Navigator para manejar la navegación
 const Stack = createStackNavigator();
 
 // Componente principal de la aplicación
 const App = () => {
+  
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => { 
+    if(requestUserPermission()){
+      messaging().getToken().then(token => {
+        console.log("Token: " + token);
+      });
+    }
+    // Check whether an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then(async(remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+        }
+      }); 
+      
+      // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(async(remoteMessage) => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );  
+    });
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+
+  }, []);
+
+
 
   const [documentData, setDocumentData] = useState(null);
-
+  
   // Retorna la estructura de navegación de la aplicación
   return (
     <NavigationContainer>
